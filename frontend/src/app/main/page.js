f176e2c84e90eb33c43808cca61d519ed3496219
WilliamCallao <96638909+WilliamCallao@users.noreply.github.com>
2024-10-15 11:43:16 -0400

    feat: install Magic UI and Button component

--- frontend/src/app/main/page.js
+++ frontend/src/app/main/page.js
@@ -1,7 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 export default function Main() {
     return (
         <div>
             <h1>Maindsdsd</h1>
+            <Button>Click me</Button>
         </div>
     );
 }
