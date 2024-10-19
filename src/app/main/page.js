55f3f4ca50df7e9f4c1ccf32e792a55b3a6d7e44
WilliamCallao <96638909+WilliamCallao@users.noreply.github.com>
2024-10-19 17:10:32 -0400

    feat: dynamically adjust font size

--- src/app/main/page.js
+++ src/app/main/page.js
@@ -1,12 +1,33 @@
 "use client";
 
 // Main.js
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import MainNavbar from "./MainNavbar";
 import heroImage from "../../assets/hero.png";
 import FeaturesSection from "./FeaturesSection";
 
 export default function Main() {
+  const redContainerRef = useRef(null);
+  const [fontSize, setFontSize] = useState("4xl");
+
+  useEffect(() => {
+    const updateFontSize = () => {
+      if (redContainerRef.current) {
+        const containerWidth = redContainerRef.current.offsetWidth;
+        const calculatedFontSize = Math.max(16, containerWidth / 5.5);
+        setFontSize(`${calculatedFontSize}px`);
+        console.log("Ancho del contenedor rojo:", containerWidth);
+      }
+    };
+
+    updateFontSize();
+    window.addEventListener("resize", updateFontSize);
+
+    return () => {
+      window.removeEventListener("resize", updateFontSize);
+    };
+  }, []);
+
   return (
     <>
       <div className="w-screen">
@@ -16,9 +37,15 @@ export default function Main() {
         className="relative flex flex-col items-center justify-center h-[calc(100vh-4rem)] bg-cover bg-center bg-no-repeat"
         style={{ backgroundImage: `url(${heroImage.src})` }}
       >
-        <div className="absolute top-0 left-0 w-[50%] h-full flex flex-col justify-center items-start px-12 lg:pl-24 lg:pr-12 bg-blue-500 bg-opacity-50">
-          <div className="w-full h-full bg-red-500 bg-opacity-50 flex flex-col justify-center items-center">
-            <h1 className="text-white text-4xl font-extrabold sm:text-5xl md:text-6xl lg:text-7xl xl:text-8xl">
+        <div className="absolute top-0 left-0 w-[58%] h-full flex flex-col justify-center items-start px-12 lg:pl-24 lg:pr-12 bg-blue-500 bg-opacity-50">
+          <div
+            ref={redContainerRef}
+            className="w-full h-full bg-red-500 bg-opacity-50 flex flex-col justify-center items-center"
+          >
+            <h1
+              className="text-white font-extrabold"
+              style={{ fontSize: fontSize }}
+            >
               CREAMOS
             </h1>
           </div>
