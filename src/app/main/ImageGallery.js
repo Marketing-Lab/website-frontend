271348a2fa33d5cd98e4d7f2fdca26553e8f9bcf
WilliamCallao <96638909+WilliamCallao@users.noreply.github.com>
2024-10-20 15:05:28 -0400

    style: adjust-height-to-width-ratio

--- src/app/main/ImageGallery.js
+++ src/app/main/ImageGallery.js
@@ -7,8 +7,8 @@ const ImageGallery = ({ images = [] }) => {
   const placeholder = "https://via.placeholder.com/400x300?text=Placeholder";
 
   return (
-    <section className="w-full h-[75vh]">
-      <div className="w-full h-full flex">
+    <section className="w-full relative" style={{ paddingTop: '35%' }}>
+      <div className="absolute top-0 left-0 w-full h-full flex">
         {Array.from({ length: 4 }).map((_, index) => (
           <div key={index} className="w-1/4 h-full">
             <img
