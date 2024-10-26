d5d88e0de9a0fc95397afa6e91de3d6391eb2d00
WilliamCallao <96638909+WilliamCallao@users.noreply.github.com>
2024-10-26 17:23:54 -0400

    Create cmsService.js

---
+++ src/app/services/cmsService.js
@@ -0,0 +1,9 @@
+// services/cmsService.js
+
+export async function getNavbarData() {
+    return {
+      logoUrl: "https://i.postimg.cc/DznMTQSV/6066b025360d7867b0b6bea116b9d7ef.png",
+      phoneNumber: "+591 69440643",
+    };
+  }
+  
